d137f4dccf6cabca7cd005f2094038babdd7c15b
Vitor de Oliveira Marques <58312845+VitorOM2@users.noreply.github.com>
2020-02-14 21:59:08 -0300

    Adicionados projetos
    Adicionados projetos simples de treinamento

---
+++ Desconto.php
@@ -0,0 +1,12 @@
+<?php
+
+    //Variaveis
+    $PrecoI = 1000;
+    $Desconto = 0.7;
+    $PrecoF = $PrecoI * $Desconto;
+
+    //Conta
+    echo "Preço inicial do produto: $PrecoI";
+    echo "<br><br>";
+    echo "Preço com desconto de 30%: $PrecoF";
+?>
---
+++ Media.php
@@ -0,0 +1,20 @@
+<?php
+    //Variaveis
+    $nota1 = 8;
+    $nota2 = 9;
+    $nota3 = 8;
+    $nota4 = 10;
+    $media = ($nota1 + $nota2 + $nota3 + $nota4)/4;
+
+    //Notas
+    echo "Nota do 1º bimestre: $nota1";
+    echo "<br>";
+    echo "Nota do 2º bimestre: $nota2";
+    echo "<br>";
+    echo "Nota do 3º bimestre: $nota3";
+    echo "<br>";
+    echo "Nota do 4º bimestre: $nota4";
+    echo "<br><br>";
+    //Média
+    echo "A média do aluno foi: $media";
+?>
